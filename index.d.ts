ce548b6b4d0ac49451d1ab11e18cf01537f5c6fc
Osman Nuri Okumus <onokumus@gmail.com>
2022-06-26 18:00:21 +0300

    remove d.ts file from root

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class MMBase {
-    private root;
-    constructor(root: Element);
-    on(evt: string, handler: EventListener, options?: AddEventListenerOptions | boolean): MMBase;
-    off(evt: string, handler: EventListener, options?: EventListenerOptions | boolean): MMBase;
-    emit<T extends object>(evt: string, evtData: T, shouldBubble?: boolean): MMBase;
-}
-export { MMBase as default };
